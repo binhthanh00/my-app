043d9b5ce622f16d35f0bb2e5acef66f000346e7
binhthanh00 <binhthanh0205@gmail.com>
2020-06-14 14:17:40 +0700

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RandomGame, { App, App2 } from './App';
+import Caculate from './caculate/Caculate'
+import JsxExtend from './jsxextend/JsxExtend'
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render( <RandomGame user_name="admin" />, document.getElementById('root'));
+// ReactDOM.render( <Caculate />, document.getElementById('root'));
+// ReactDOM.render( <JsxExtend />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
