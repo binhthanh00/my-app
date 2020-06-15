6714db48d57fd1a3c57bd0d7f00dcfdf46eeee28
binhthanh00 <binhthanh0205@gmail.com>
2020-06-15 22:19:49 +0700

    code 15/6

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import RandomGame, { App, App2 } from './App';
 import Caculate from './caculate/Caculate'
 import JsxExtend from './jsxextend/JsxExtend'
+import Parent from './lifecyle/Parent'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <RandomGame user_name="admin" />, document.getElementById('root'));
+// ReactDOM.render( <RandomGame user_name="admin" />, document.getElementById('root'));
+ReactDOM.render( <Parent user_name="admin" />, document.getElementById('root'));
 // ReactDOM.render( <Caculate />, document.getElementById('root'));
 // ReactDOM.render( <JsxExtend />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
